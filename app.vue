29f5eefd172711d92aeee5512c1878f7e4fd2f4e
hikaru <alclimb@users.noreply.github.com>
2022-05-17 09:37:49 +0900

    feat: 実験用のページを追加

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <!-- メニュー -->
+  <ul>
+    <li>
+      <NuxtLink to="/">Index</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/about">About</NuxtLink>
+    </li>
+  </ul>
+
+  <!-- ページ内容の表示 -->
+  <NuxtPage />
 </template>
